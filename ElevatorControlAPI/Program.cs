d8a458d75b09562c4c727538e620040c57eda2e5
Donald Meldrum <donald.meldrum@gmail.com>
2023-07-20 20:24:27 -0700

    Added default ASP.net Core Web API project Added .gitignore

---
+++ ElevatorControlAPI/Program.cs
@@ -0,0 +1,27 @@
+using Microsoft.AspNetCore.Authentication.Negotiate;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+
+builder.Services.AddAuthentication(NegotiateDefaults.AuthenticationScheme)
+   .AddNegotiate();
+
+builder.Services.AddAuthorization(options =>
+{
+    // By default, all incoming requests will be authorized according to the default policy.
+    options.FallbackPolicy = options.DefaultPolicy;
+});
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+
+app.UseAuthentication();
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
